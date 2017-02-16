7b2f027e316bef266b962f050678fb0b3a1a1b12
abdultw <abdultw@dukes.jmu.edu>
2017-02-16 01:36:15 -0500

    Added MyTime Xcode Project

---
+++ MyTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyTime
+//
+//  Developers: Austin Vershel, Tughral Abdul
+//
+//  Created by Tughral Abdul on 2/16/17.
+//  Copyright © 2017 MyTime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
